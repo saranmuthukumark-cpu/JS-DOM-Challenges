fb92c7a681a7338a132922fc079fa3603ed85284
saranmuthukumark-cpu <saranmuthukumar.k@gmail.com>
2026-01-21 09:39:45 +0530

    text formatter updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./*.html"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
-}
-=======
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
->>>>>>> 1cda5223dea3e932aff09061b9e7b8c8b1cda55f
+};
